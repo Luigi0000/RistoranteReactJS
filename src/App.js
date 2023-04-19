9681f05b3a2d1e343c6711523a72b3917288bd85
Luigi0000 <luigi_diluca1994@libero.it>
2023-04-19 11:54:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Card from './components/card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import lasagna from './images/lasagna.jpg';
+import bolognese from './images/bolognese.jpg';
+import spaghetti from './images/spaghetti.jpg';
+import pastapatate from './images/pastapatate.jpg';
+
+class App extends Component {
+  state = {
+    cards: [
+      { id: 0, nome: "Lasagna", prezzo: 1.99, immagine: lasagna, quantità: 0 },
+      { id: 1, nome: "Tagliatelle alla Bolognese", prezzo: 3.19, immagine: bolognese, quantità: 0 },
+      { id: 2, nome: "Speghetti alle vongole", prezzo: 2.49, immagine: spaghetti, quantità: 0 },
+      { id: 3, nome: "Pasta e patate", prezzo: 4.99, immagine: pastapatate, quantità: 0 }
+    ]
+  }
+  /* Elimina Card */
+  handleDelete = cardId => {
+    const cards = this.state.cards.filter(card => card.id !== cardId);
+    this.setState({ cards });
+  }
+  /* Aggiunta Quantità */
+  handleIncrement = card => {
+    const cards = [...this.state.cards];
+    const id = cards.indexOf(card);
+    cards[id] = { ...card };
+    cards[id].quantità++;
+    this.setState({ cards });
+
+  }
+  resetCards = cards => {
+    this.setState({ cards });
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className='container'>
+          <h1>Il nostro menù</h1>
+          <hr />
+          <div className='row'>
+            {this.state.cards.map(card => (
+              <Card
+                key={card.id}
+                onDelete={this.handleDelete}
+                onIncrement={this.handleIncrement}
+                card={card} />
+
+            ))}
+          </div>
+        </div>
+
+
+      </>
+    );
+  }
 }
-
 export default App;
